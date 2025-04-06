95c92b38cf76ea0a807774e59eb73ed4cb558f5f
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-04-06 12:11:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,36 +10,33 @@ function convertToRoman(num) {
     };
 
   //your code here
-	let result = "";
-	const symbols = Object.values(obj);
+	let result = ""; // This will store the final Roman numeral
 
-	// Manually add subtractive cases
-	const subtractive = {
-		900: "CM",
-		400: "CD",
-		90: "XC",
-		40: "XL",
-		9: "IX",
-		4: "IV"
-	};
+	// Step 1: First, handle subtractive cases
+	for (let key in subtractiveCases) {
+		let value = parseInt(key); // convert key string to number
+		let symbol = subtractiveCases[key];
 
-	// Handle subtractive cases first
-	for (let value in subtractive) {
-		while (num >= value) {
-			result += subtractive[value];
-			num -= value;
-		}
-	}
-
-	// Then handle remaining values
-	for (let [symbol, value] of symbols) {
 		while (num >= value) {
 			result += symbol;
 			num -= value;
 		}
 	}
 
-	return result;
+	// Step 2: Handle the normal Roman numerals
+	const valuesArray = Object.values(obj); // convert object to array
+
+	for (let i = 0; i < valuesArray.length; i++) {
+		let symbol = valuesArray[i][0];
+		let value = valuesArray[i][1];
+
+		while (num >= value) {
+			result += symbol;
+			num -= value;
+		}
+	}
+
+	return result; 
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
